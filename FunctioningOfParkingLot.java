19927ffc36c489ec3e20cf628dfe7b2e1df2935b
akash-madugundi <128323016+akash-madugundi@users.noreply.github.com>
2023-09-23 00:44:54 +0530

    Update FunctioningOfParkingLot.java

--- FunctioningOfParkingLot.java
+++ FunctioningOfParkingLot.java
@@ -9,6 +9,12 @@ public class FunctioningOfParkingLot {
         System.out.println(" 2 to remove Vehicle ");
         System.out.println(" -1 to end");
     }
+    public static void Instructions()
+    {
+        System.out.println(" 1 To management view ");
+        System.out.println(" 2 To customer view ");
+        System.out.println(" -1 to end");
+    }
     public static void ChooseVehicle()
     {
         System.out.println(" 1 To bike ");
@@ -25,23 +31,48 @@ public class FunctioningOfParkingLot {
         int NoOfFloors = S2.nextInt();
         Management M = new Management(NoOfFloors);
         User U = new User();
-        PreliminaryInstructions();
-        int x = S2.nextInt();
-        while(x!=-1)
+        String Password = "PassWord";
+        Instructions();
+        int x9 = S2.nextInt();
+        while(x9!= -1)
         {
-            ChooseVehicle();
-            if(x==1)
+            if(x9==1)
             {
-                int ChoiceOfVehicle = S2.nextInt(),FloorNumber=S2.nextInt();
-                U.Enter(M, FloorNumber-1, ChoiceOfVehicle);
-            }
-            else if(x==2)
+                System.out.println("Enter Password");
+                String GivenPassword = S2.next();
+                if(Password.compareTo(GivenPassword)==0)
+                {
+                    System.out.println("Enter the floor and Slot of floor to Block ");
+                    int Floor = S2.nextInt(),slotId = S2.nextInt();
+                    M.ManageSlots(Floor,slotId,false);
+                }
+                else
+                {
+                    System.out.println("wrong password , Try again");
+                }
+            } else if (x9==2)
             {
-                int ChoiceOfVehicle = S2.nextInt();
-                U.Exit(M, ChoiceOfVehicle);   
+                PreliminaryInstructions();
+                int x = S2.nextInt();
+                while(x!=-1)
+                {
+                    ChooseVehicle();
+                    if(x==1)
+                    {
+                        int ChoiceOfVehicle = S2.nextInt(),FloorNumber=S2.nextInt();
+                        U.Enter(M, FloorNumber-1, ChoiceOfVehicle);
+                    }
+                    else if(x==2)
+                    {
+                        int ChoiceOfVehicle = S2.nextInt();
+                        U.Exit(M, ChoiceOfVehicle);
+                    }
+                    PreliminaryInstructions();
+                    x =S2.nextInt();
+                }
             }
-            PreliminaryInstructions();
-            x =S2.nextInt();
+            Instructions();
+            x9 = S2.nextInt();
         }
         S2.close();
     }
