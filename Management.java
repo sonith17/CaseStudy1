93e56db66b1fbb55c4455eb7b27a82b2ba390bd9
sonith17 <128211909+sonith17@users.noreply.github.com>
2023-09-22 22:51:46 +0530

    User and Mangement added

--- Management.java
+++ Management.java
@@ -1,1 +1,27 @@
+package caseStudyV1_1;
+import java.util.*;
 
+public class Management 
+{
+    Floor F[];
+    LinkedList<Vehicle> ListVehicles;
+    Management(int NoOfFloors)
+    {
+        ListVehicles = new LinkedList<>();
+        this.F= new Floor[NoOfFloors];
+        for(int i=0;i<NoOfFloors;i++)
+        {
+            Scanner S1 =new Scanner(System.in);
+            System.out.println("enter No of Entries,Exits,SlotsX,SlotY,Cost,NoOfCars,Trucks,Bikes,costPerUnitCharge Respectively");
+            int Entries =S1.nextInt(),Exits=S1.nextInt(),SlotsX=S1.nextInt(),SlotsY = S1.nextInt(),cost =S1.nextInt(),NoOfCars=S1.nextInt(),NoOfTrucks=S1.nextInt(),NoOfBikes=S1.nextInt(),costPerCharge=S1.nextInt();
+            F[i]= new Floor(SlotsX,SlotsY,Entries,Exits,cost,NoOfCars,NoOfBikes,NoOfTrucks,costPerCharge);
+ //           S1.close();
+        }
+    }
+    void ManageSlots(int FloorNumber,int SlotId,boolean SlotStatus)
+    {
+        int xcordinate = --SlotId/(F[FloorNumber].s[0].length);
+        int ycordinate =SlotId%(F[FloorNumber].s[0].length);
+        F[FloorNumber].s[xcordinate][ycordinate].SlotStatusSetter(SlotStatus);
+    }
+}
--- User.java
+++ User.java
@@ -1,1 +1,138 @@
+package caseStudyV1_1;
+
+import java.util.Scanner;
+
+public class User
+{
+    private String VehicleName;
+    private String PlateNumber;
+
+    void VehicleNameSetter(String VehicleName)
+    {
+        this.VehicleName = VehicleName;
+    }
+
+    String VehicleNameGetter()
+    {
+        return this.VehicleName;
+    }
+    void PlateNumberSetter(String PlateNumber)
+    {
+        this.PlateNumber = PlateNumber;
+    }
+
+    String PlateNumberGetter()
+    {
+        return this.PlateNumber;
+    }
+    
+    void Enter(Management M, int FloorNumber,int ChoiceOfVehicle)
+    {
+        Scanner S = new Scanner(System.in);
+        if(M.F[FloorNumber].CheckFloorEmpty())
+                    { 
+                        System.out.println("Enter your Full Name ");
+                        String Name = S.next();
+                        Vehicle B[] = new Vehicle[4];
+                        B[0]= new Bike(Name);
+                        B[1]= new Car(Name);
+                        B[3]= new Truck(Name);
+                        B[2]= new ElectricCar(Name);
+                        M.F[FloorNumber].DisplayEntries();
+                        int x3 = S.nextInt()-1;
+                        M.F[FloorNumber].en[x3].IsEmpty= false;
+                        M.F[FloorNumber].en[x3].prePaymentOptions();
+                        int x4 = S.nextInt();
+                        if(x4!= -1)
+                        {
+                            System.out.println(" Enter Advance You wish To pay ");
+                            int Advance = S.nextInt();
+                            M.F[FloorNumber].en[x3].PrePayment(Advance, B[ChoiceOfVehicle-1]);
+                        }
+                        M.F[FloorNumber].en[x3].AssignInTime(B[ChoiceOfVehicle-1]);
+                        M.F[FloorNumber].DisplaySlots();
+                        int slot = M.F[FloorNumber].AllocateParkingLot(B[ChoiceOfVehicle-1]);
+                        int xcordinate = --slot/(M.F[FloorNumber].s[0].length);
+                        int ycordinate =slot%(M.F[FloorNumber].s[0].length);
+                        M.F[FloorNumber].s[xcordinate][ycordinate].RemainingSpace-=1;
+                        if(M.F[FloorNumber].s[xcordinate][ycordinate].RemainingSpace==0)
+                        {
+                            M.F[FloorNumber].s[xcordinate][ycordinate].IsSlotEmpty=false;
+                        }
+                        M.F[FloorNumber].en[x3].IsEmpty= true;
+                        M.ListVehicles.add(B[ChoiceOfVehicle-1]);
+                    }
+        else{System.out.println("Floor is full choose again");}
+ //       S.close();
+    }
+
+    void Exit(Management M,int ChoiceOfVehicle)
+    {
+        Scanner S11 = new Scanner(System.in);
+        System.out.println(" Enter The Floor you Parked Your Vehicle");
+                    int x2 = S11.nextInt()-1;
+                    while(x2>M.F.length || x2<0)
+                    {
+                        System.out.println("Enter Floor again (you entered invalid Floor)");
+                        x2 = S11.nextInt()-1;
+                    }
+                    int slot = 0,VehicleIdInList= -1;
+                    boolean VehicleIsPresent=true;
+                    while(VehicleIdInList<0 && VehicleIsPresent)
+                    {
+                        System.out.println("Enter Your Your Full Name as registerd in Info");
+                        String Name = S11.next();
+                        for(int i=0;i<M.ListVehicles.size();i++)
+                        {
+                            if(M.ListVehicles.get(i).OwnerName.compareTo(Name)==0)
+                            {
+                                slot = M.ListVehicles.get(i).AllocatedParkingLot;
+                                VehicleIdInList= i;
+                                break;
+                            }
+                        }
+                        if(VehicleIdInList==-1)
+                        {
+                            System.out.println("Vehicle not found , Enter name Correctly");
+                            System.out.println("If Your Vehicle is not parked ,Enter -2 ");
+                        }
+                        if(VehicleIdInList==-2)
+                        {
+                            VehicleIsPresent=false;
+                        }
+                    }
+                    if(VehicleIdInList>=0)
+                    {
+                        System.out.println("Choose Exit");
+                        M.F[x2].DisplayExits();
+                        int x3 = S11.nextInt()-1;
+                        M.F[x2].ex[x3].IsEmpty=false;
+                        M.F[x2].ex[x3].AssignOutTime(M.ListVehicles.get(VehicleIdInList));
+                        slot = M.ListVehicles.get(VehicleIdInList).AllocatedParkingLot;
+                        int xcordinate = --slot/(M.F[x2].s[0].length);
+                        int ycordinate =slot%(M.F[x2].s[0].length);
+                        M.F[x2].s[xcordinate][ycordinate].MakeSlotFree(1);
+                        M.F[x2].ex[x3].CheckPayment(M.ListVehicles.get(VehicleIdInList),M.F[x2].cost);
+                        M.F[x2].ex[x3].PaymentOptions();
+                        int x4 =S11.nextInt();
+                        while(x4!=1&& x4!=2)
+                        {
+                            System.out.println("Invalid Payment Method,Enter Choice Again");
+                            x4=S11.nextInt();
+                        }
+                        if(x4==1)
+                        {
+                            M.ListVehicles.get(VehicleIdInList).PaymentMethod="Card";
+                        }
+                        else if(x4==2)
+                        {
+                            M.ListVehicles.get(VehicleIdInList).PaymentMethod="Attendant";
+                        }
+                        M.ListVehicles.get(VehicleIdInList).PayFee();
+                        M.F[x2].ex[x3].CheckPayment(M.ListVehicles.get(VehicleIdInList),M.F[x2].cost);
+                        M.F[x2].ex[x3].IsEmpty=true;
+                    }
+   //                 S11.close();
+    }
+}
 
